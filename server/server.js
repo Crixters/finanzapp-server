c497571b31c693fad25750199f144a3b5a7bed0a
Crixters <cdgm09@hotmail.com>
2020-12-02 21:21:47 -0500

    login and register

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('./config/config');
 
-app.use(require('./rutas/presentadorRutas'));
+app.use(require('./rutas/presentadorRutas'));//
 
 app.listen(process.env.PORT, () => {
     console.log("Corriendo en el puerto " + process.env.PORT);
